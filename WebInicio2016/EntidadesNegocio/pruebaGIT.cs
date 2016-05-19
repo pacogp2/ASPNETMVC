52e0940d3ea62aafebf6887e3905b5149ba66d49
pacogp2 <pacogp2@gmail.com>
2016-05-19 16:13:55 +0200

    nueva clase pruebaGIT

---
+++ WebInicio2016/EntidadesNegocio/pruebaGIT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebInicio2016.EntidadesNegocio
+{
+    public class pruebaGIT
+    {
+        public int IdEmpleado { get; set; }
+        public Boolean OK { get; set; }
+        public String mensaje { get; set; }
+    }
+}
